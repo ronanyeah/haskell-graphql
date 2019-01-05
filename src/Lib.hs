ffc3cbc25286629a054a3fb08c01005f1692ea8f
Ronan <ronan_mccabe@protonmail.com>
2019-01-05 05:27:49 +0000

    working gql hello world

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
+
 module Lib
-  ( someFunc
+  ( hello
+  , run
   ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Monoid      ((<>))
+import           Data.Text        (Text)
+
+import           GraphQL
+import           GraphQL.API
+import           GraphQL.Resolver (Handler)
+
+type Hello = Object "Hello" '[] '[ Argument "who" Text :> Field "greeting" Text]
+
+hello :: Handler IO Hello
+hello = pure (\who -> pure ("Hello " <> who))
+
+run :: Text -> IO Response
+run = interpretAnonymousQuery @Hello hello
